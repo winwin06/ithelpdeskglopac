ae4e3118e74358e9afb486b359e0227b18c92996
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-02-27 16:05:16 +0700

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -43,27 +43,26 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+         
+        <li class="nav-item has-treeview menu-open">
+            <a href="<?= base_url('dashboard')?>" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                Dashboard
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
-          
+
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-users"></i>
                 <p>Department</p>
             </a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-users"></i>
                 <p>Job Request</p>
             </a>
