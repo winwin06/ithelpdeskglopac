a702e392c478ff1a8c0bf33f1824ef617eb58785
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-02-28 14:54:23 +0700

    Update register.php

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -35,7 +35,6 @@
                         <input type="text" class="form-control" placeholder="Full name">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-user"></span>
                             </div>
                         </div>
                     </div>
@@ -43,7 +42,6 @@
                         <input type="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
                             </div>
                         </div>
                     </div>
@@ -51,7 +49,6 @@
                         <input type="password" class="form-control" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
                             </div>
                         </div>
                     </div>
@@ -59,39 +56,22 @@
                         <input type="password" class="form-control" placeholder="Retype password">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                <label for="agreeTerms">
-                                    I agree to the <a href="#">terms</a>
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                        <!-- /.col -->
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
                     </div>
-                </form>
-
-                <div class="social-auth-links text-center">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i>
-                        Sign up using Google+
-                    </a>
-                </div>
-
-                <a href="login.html" class="text-center">I already have a membership</a>
+                    <!-- /.col -->
             </div>
-            <!-- /.form-box -->
-        </div><!-- /.card -->
+            </form>
+
+
+            <a href="login.html" class="text-center">I already have a membership</a>
+        </div>
+        <!-- /.form-box -->
+    </div><!-- /.card -->
     </div>
     <!-- /.register-box -->
 
@@ -102,4 +82,5 @@
     <!-- AdminLTE App -->
     <script src="../../dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
