85353df40e1ffa1c0c50af8f4278bd62194778b0
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-02-28 14:44:52 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -25,64 +25,39 @@
         <div class="login-logo">
             <a href="../../index2.html"><b>Login</b>Glp</a>
         </div>
-        <!-- /.login-logo -->
+
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+                <p class="login-box-msg">Sign in Now!!</p>
 
                 <form action="../../index3.html" method="post">
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="email" placeholder="Email">
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
 
-                <div class="social-auth-links text-center mb-3">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>
-                <!-- /.social-auth-links -->
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="password" placeholder="Password">
+                    </div>
 
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+            </div>
+            </form>
+
+            <div class="text-center">
+                <p class="form-group">
+                    <a href="forgot-password.html">Forgot Password?</a>
                 </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
+                <p class="form-group">
+                    <a href="register.html" class="text-center">Create an Account!</a>
                 </p>
             </div>
-            <!-- /.login-card-body -->
+
         </div>
     </div>
-    <!-- /.login-box -->
+    </div>
+
 
     <!-- jQuery -->
     <script src="../../plugins/jquery/jquery.min.js"></script>
