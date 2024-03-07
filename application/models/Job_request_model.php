46e394ae41855f4fe2248ff8037f7f0ef081a1d7
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-03-07 16:30:34 +0700

    Update Job_request_model.php

--- application/models/Job_request_model.php
+++ application/models/Job_request_model.php
@@ -3,9 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Job_request_model extends CI_Model
 {
-
     public function get_data($table)
-    {
-        return $this->db->get($table);
-    }
+	{
+		return $this->db->get($table);
+	}
 }
