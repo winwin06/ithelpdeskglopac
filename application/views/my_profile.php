3ec79a03f079c51470dd44f7774f631c2d08236e
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-03-22 16:49:07 +0700

    Update my_profile.php

--- application/views/my_profile.php
+++ application/views/my_profile.php
@@ -1,8 +1,9 @@
 <div class="card mb-3" style="max-width: 540px;">
     <div class="row no-gutters">
-        <!-- <div class="col-md-4">
-            <img src="<?= base_url('assets/dist/img/profile/') ?>">
-        </div> -->
+        <div class="col-md-4">
+        <img src="<?= base_url('assets/dist/img/profile/profile.png') ?>" 
+        class="card-img" style="max-width: 100%; height: auto;">
+        </div>
         <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title"><?= $user['name']; ?></h5>
