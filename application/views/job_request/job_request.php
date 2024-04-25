2b407dbe6cb923da6e0a15dd3aa2459952e15614
winwin06 <wiwin21ti@mahasiswa.pcr.ac.id>
2024-04-25 16:12:25 +0700

    Update job_request.php

--- application/views/job_request/job_request.php
+++ application/views/job_request/job_request.php
@@ -17,10 +17,51 @@
     <div class="row">
         <div class="col-md-6">
             <a href="<?= site_url('job_request/create_job') ?>" class="btn btn-info mb-2"><i class="fas fa-plus-circle"></i> Create</a>
-            <a href="<?= site_url('job_request/create_job') ?>" class="btn btn-info mb-2"><i class="fas fa-search"></i> Filter</a>
+            <button type="button" class="btn btn-info mb-2" data-toggle="modal" data-target="#filterModal"><i class="fas fa-filter"></i> Filter</button>
         </div>
         <?= $this->session->flashdata('message') ?>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="filterModal" tabindex="-1" role="dialog" aria-labelledby="filterModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="filterModalLabel">Filter</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form untuk filter -->
+                    <form action="<?= site_url('job_request') ?>" method="post">
+                        <div class="form-group">
+                            <label for="dateFrom">Date From</label>
+                            <input type="date" class="form-control" id="created_at" name="created_at">
+                        </div>
+                        <div class="form-group">
+                            <label for="dateTo">Date To</label>
+                            <input type="date" class="form-control" id="created_at" name="creayed_at">
+                        </div>
+                        <div class="form-group">
+                            <label for="status">Status</label>
+                            <select class="form-control" id="status" name="status">
+                                <option value="">Select Status</option>
+                                <option value="Not Started">Not Started</option>
+                                <option value="On Going">On Going</option>
+                                <option value="Done">Done</option>
+                            </select>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Search</button>
+                            <button type="button" class="btn btn-secondary text-white border-0" data-dismiss="modal">Close</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     
     <div class="card">
         <div class="card-header">
@@ -53,7 +94,7 @@
                             <td><?= $us['status']; ?></td>
                             <td>
                                 <a href="<?= site_url('job_request/detail_job/') . $us['id']; ?>" class="badge badge-info">Detail</a>
-                                <!-- <a href="<?= site_url('job_request/edit_job/') . $us['id']; ?>" class="badge badge-warning">Edit</a> -->
+                              
                                 <?php if ($this->session->userdata("role") == "admin" || $us['status'] == 'Not Started') : ?>
                                     <a href="<?= site_url('job_request/edit_job/') . $us['id']; ?>" class="badge badge-warning">Edit</a>
                                 <?php endif; ?>
